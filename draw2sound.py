bc79351f5031625be3ad6a2f2012c1bab298d3bb
Chris-Ades <ca2972@columbia.edu>
2024-04-13 13:58:35 -0400

    frequency mode works

--- draw2sound.py
+++ draw2sound.py
@@ -1,9 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QLabel
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QColorDialog, QVBoxLayout, QWidget, QLabel, QPushButton, QMessageBox, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QColorDialog, QVBoxLayout, QWidget, QLabel, QPushButton, QMessageBox, QDesktopWidget, QTabWidget
 from PyQt5.QtGui import QPainter, QPen, QColor, QPainterPath
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 import numpy as np
 import pyaudio
 from astropy.convolution import convolve, Box1DKernel
@@ -59,15 +57,8 @@ class Draw2SoundGUI(QMainWindow):
     def closeEvent(self, event):
         if (self.time_canvas.pyo_server.getIsBooted):
             self.time_canvas.pyo_server.shutdown()
-            #also shut down all Scope, Spectrum, Keyboard windows
-        
         print("shutting down")
         event.accept()
-
-    # def tab_changed(self, index):
-    #     # Get the index of the currently selected tab
-    #     current_tab_index = self.tab_widget.currentIndex()
-    #     print("Tab changed to:", current_tab_index)
 
 class Canvas(QWidget):
     def __init__(self, parent, domain):
@@ -80,9 +71,21 @@ class Canvas(QWidget):
         self.drawingEnabled = True 
         self.pen = QPen(Qt.black, 2)
         self.path = QPainterPath()
-        self.lastPoint = None
-        self.x_values = []
-        self.y_values = []
+
+        if self.domain == 'time':
+            self.lastPoint = None
+            self.x_values = []
+            self.y_values = []
+            self.max_x = 10000
+
+        if self.domain == 'frequency':
+            self.position = []
+            self.current_drag_point = None
+            self.drag_offset = None
+            self.diameter = 8
+            self.mag_norm = 700
+            self.max_x = 10000
+
         self.mouse_position_label = QLabel(self)
         self.mouse_position_label.move(10, 10)  
         self.mouse_position_label.setText("Mouse Position: (0, 0)")
@@ -91,9 +94,7 @@ class Canvas(QWidget):
 
         self.pyo_server = parent.pyo_server
         self.notes = parent.notes
-        self.max_freq = 4096 # maximum frequency one can draw in frequency mode
 
-        # Play sound button
         move_x = self.width()
         self.chooseSoundButton = QPushButton('Choose Sound', self)
         self.chooseSoundButton.clicked.connect(self.chooseSound)
@@ -166,72 +167,112 @@ class Canvas(QWidget):
             painter.setPen(QPen(Qt.gray, centerLineThickness, Qt.SolidLine))
             painter.drawLine(0, self.height(), self.width(), self.height())
             painter.drawLine(0, 0, 0, self.height()) 
+            
+            painter.setPen(self.pen)
+            for point in self.position:
+                x, y = point
+                painter.setBrush(Qt.black)
+                painter.drawEllipse(QPoint(x, y), self.diameter, self.diameter)
+                text_position = QPoint(x + 10, y - 10)
+                painter.drawText(text_position, f"({int(round(x * self.max_x / self.width(), 0))}, {(self.height() - y)/self.mag_norm})")
+
+                painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+                painter.drawLine(x, y, x, self.height())
 
     def mousePressEvent(self, event):
-        if self.drawingEnabled and event.button() == Qt.LeftButton:
-            self.lastPoint = event.pos()
-            self.path.moveTo(self.lastPoint)
-            self.add_point(event.pos())
+        if self.domain == "time":
+            if self.drawingEnabled and event.button() == Qt.LeftButton:
+                self.lastPoint = event.pos()
+                self.path.moveTo(self.lastPoint)
+                self.add_point(event.pos())
+
+        if self.domain == "frequency":
+            for i, point in enumerate(self.position):
+                if abs(point[0] - event.x()) <= self.diameter and abs(point[1] - event.y()) <= self.diameter:
+                    self.current_drag_point = i
+                    self.drag_offset = QPoint(event.pos() - QPoint(point[0], point[1]))
+                    return
+
+            self.position.append((event.x(), event.y()))
+            self.update()
+            self.chooseSoundButton.setEnabled(True)
 
     def mouseMoveEvent(self, event):
         if self.domain == "time":
             x = event.x() - self.width() / 2
             y = event.y() - self.height() / 2
+            self.mouse_position_label.setText(f"({x}, {y})")
+            if self.drawingEnabled and event.buttons() & Qt.LeftButton:
+                if event.pos().x() > self.lastPoint.x():
+                    self.path.lineTo(event.pos())
+                    self.update()
+                    self.lastPoint = event.pos()
+                    self.add_point(event.pos())
+
         if self.domain == "frequency":
-            x = int(round(event.x() * self.max_freq / self.width(), 0))
-            y = self.height() - event.y()
-        self.mouse_position_label.setText(f"({x}, {y})")
-        if self.drawingEnabled and event.buttons() & Qt.LeftButton:
-            if event.pos().x() > self.lastPoint.x():
-                self.path.lineTo(event.pos())
+            x = int(round(event.x() * self.max_x / self.width(), 0))
+            y = (self.height() - event.y())/self.mag_norm
+            self.mouse_position_label.setText(f"({x}, {y})")
+
+            if self.current_drag_point is not None:
+                self.position[self.current_drag_point] = (event.x() - self.drag_offset.x(),
+                                                            event.y() - self.drag_offset.y())
                 self.update()
-                self.lastPoint = event.pos()
-                self.add_point(event.pos())
 
     def mouseReleaseEvent(self, event):
-        if self.drawingEnabled and event.button() == Qt.LeftButton:
-            self.chooseSoundButton.setEnabled(True)
-            self.path.lineTo(event.pos())
-            self.update()
-            self.drawingEnabled = False 
+        if self.domain == "time":
+            if self.drawingEnabled and event.button() == Qt.LeftButton:
+                self.chooseSoundButton.setEnabled(True)
+                self.path.lineTo(event.pos())
+                self.update()
+                self.drawingEnabled = False
+
+        if self.domain == "frequency":
+            if event.button() == Qt.LeftButton:
+                self.current_drag_point = None
+
+    def mouseDoubleClickEvent(self, event):
+        if self.domain == "frequency":
+            for i, point in enumerate(self.position):
+                if abs(point[0] - event.x()) <= self.diameter and abs(point[1] - event.y()) <= self.diameter:
+                    del self.position[i]
+                    self.update()
+                    if self.position == []:
+                        self.chooseSoundButton.setEnabled(False)
+                    return
 
     def clearCanvas(self):
-        self.path = QPainterPath()
-        self.update()
-        self.drawingEnabled = True
-        self.x_values = []
-        self.y_values = []
-
         self.chooseSoundButton.setEnabled(False)
         self.scopeButton.setEnabled(False)
         self.spectrumButton.setEnabled(False)
         self.keyboardButton.setEnabled(False)
 
         if self.domain == "time":
+            self.path = QPainterPath()
+            self.update()
+            self.drawingEnabled = True
+            self.x_values = []
+            self.y_values = []
             self.parent.tab_widget.setTabEnabled(1, True)
         
         if self.domain == "frequency":
+            self.position = []
             self.parent.tab_widget.setTabEnabled(0, True)
+            self.update()
 
         if self.pyo_server.getIsStarted():
             self.a.stop()
             self.pyo_server.stop()
 
     def add_point(self, pos):
-
         if self.domain == "time":
             self.x_values.append(pos.x() - self.width() / 2)
             self.y_values.append(pos.y() - self.height() / 2)
 
-        if self.domain == "frequency":
-            self.x_values.append(int(round(pos.x() * self.max_freq / self.width(), 0)))
-            self.y_values.append(self.height() - pos.y())
-
     def chooseSound(self):
-        if (self.y_values == []):
-            return
-
         if self.domain == "time":
+            if (self.y_values == []):
+                return
             self.parent.tab_widget.setTabEnabled(1, False)
             # interpolate values like you did so you can increase sampling frequency
             # scale time domain and add number markers
@@ -267,27 +308,8 @@ class Canvas(QWidget):
 
         if self.domain == "frequency":
             self.parent.tab_widget.setTabEnabled(0, False)
-            x = np.array(self.x_values)
-            y = np.array(self.y_values)
-            y[y < 0] = 0
-            max_norm = np.max(y)
-            y = y / max_norm
 
-            x_new = np.arange(x[0], x[-1]+1)
-            y_new = np.interp(x_new, x, y)
-            first_elem = x[0]
-            last_elem = x[-1]
-            
-            amplitudes = y_new
-            if first_elem != 0:
-                pad_left = np.zeros(first_elem - 1)
-                amplitudes = np.concatenate((pad_left, y_new))
-
-            if last_elem != self.max_freq:
-                pad_right = np.zeros(self.max_freq - last_elem)
-                amplitudes = np.concatenate((amplitudes, pad_right))
-
-            frequencies = np.arange(1, self.max_freq + 1)
+            data_points = [(int(round(x * self.max_x / self.width(), 0)), (self.height() - y)/self.mag_norm) for x, y in self.position]
 
             #######################################################################
             N = 44100
@@ -297,32 +319,21 @@ class Canvas(QWidget):
 
             waveform = np.zeros_like(t)
 
-            start_time = time.time()
-            for i in range(self.max_freq):
-                if amplitudes[i] != 0:
-                    waveform = waveform + (amplitudes[i]*np.sin(frequencies[i] * t))
-            end_time = time.time()
+            for point in data_points:
+                freq, magnitude = point
+                if magnitude != 0:
+                    waveform = waveform + (magnitude*np.sin(freq * t))
 
-            fund_freq = frequencies[np.argmax(amplitudes)]
-
-            elapsed_time = end_time - start_time
-            print(f"Elapsed time for {10000} iterations: {elapsed_time} seconds")
+            fund_freq = max(data_points, key=lambda x: x[1])[0]
 
             duration = 5
             waveform = np.tile(waveform, duration) 
             waveform = waveform / np.max(waveform)
-            # Plot the sine wave
-            plt.plot(waveform)
-            plt.xlabel('Time')
-            plt.ylabel('Amplitude')
-            plt.title('One Period of a Sine Wave')
-            plt.grid(True)
-            plt.show()
             
             ###########################################################################
             signal_int16 = np.int16(waveform * 32767)
             write(self.file_name, sampling_freq, signal_int16)
-            #self.show_fft(waveform, sampling_freq)
+            self.show_fft(waveform, sampling_freq)
 
             self.pyo_server.start()
             self.start_synth(fund_freq, loop=True)
@@ -355,7 +366,6 @@ class Canvas(QWidget):
         return
     
     def show_keyboard(self):
-        #checks if there is a Scope window already open
         self.notes.keyboard(title="Keyboard", wxnoserver=True)
     
     def show_fft(self, waveform, sampling_freq):
@@ -373,6 +383,7 @@ class Canvas(QWidget):
 
 def main():
     app = QApplication(sys.argv)
+    app.setDoubleClickInterval(200)
     ex = Draw2SoundGUI()
     ex.show()
     #ex.showFullScreen()
