22c51115c4a7afcf44e21843b77ed9801bc37e8e
Chris-Ades <ca2972@columbia.edu>
2024-04-13 17:33:16 -0400

    added quare wave preset

--- draw2sound.py
+++ draw2sound.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QColorDialog, QVBoxLayout, QWidget, QLabel, QPushButton, QMessageBox, QDesktopWidget, QTabWidget
-from PyQt5.QtGui import QPainter, QPen, QColor, QPainterPath
+from PyQt5.QtGui import QPainter, QPen, QColor, QPainterPath, QFont
 from PyQt5.QtCore import Qt, QPoint
 import numpy as np
 import pyaudio
@@ -32,6 +32,7 @@ class Draw2SoundGUI(QMainWindow):
         self.notes = Notein(poly=16, scale=1, first=0, last=127, channel=0, mul=1)
 
         self.createTabs()
+        self.createMenu()
 
     def createTabs(self):
         self.tab_widget = QTabWidget()
@@ -53,12 +54,37 @@ class Draw2SoundGUI(QMainWindow):
         self.frequency_canvas = Canvas(self, 'frequency')
         layout2.addWidget(self.frequency_canvas)
         self.tab2.setLayout(layout2)
+    
+    def createMenu(self):
+        presets_menu = self.menuBar().addMenu("Presets")
+        square_action = QAction("Square", self)
+        square_action.triggered.connect(self.add_square_preset)
+        presets_menu.addAction(square_action)
+        if self.tab_widget.currentIndex() == 1:
+            pass
+
+    def add_square_preset(self):
+        if self.tab_widget.currentIndex() == 1:
+            self.frequency_canvas.position = []
+            
+            num_terms = 400
+            fundamental_freq = 100
+            for n in range(1, num_terms + 1):
+                amplitude = 4 / (n * np.pi)
+                frequency = n * fundamental_freq
+                self.frequency_canvas.position.append((self.frequency_canvas.x_transform_inverse(frequency), self.frequency_canvas.y_transform_inverse(amplitude)))
+
+            self.frequency_canvas.update()
+            self.frequency_canvas.chooseSoundButton.setEnabled(True)
+
 
     def closeEvent(self, event):
         if (self.time_canvas.pyo_server.getIsBooted):
             self.time_canvas.pyo_server.shutdown()
         print("shutting down")
         event.accept()
+
+    
 
 class Canvas(QWidget):
     def __init__(self, parent, domain):
@@ -76,21 +102,20 @@ class Canvas(QWidget):
             self.lastPoint = None
             self.x_values = []
             self.y_values = []
-            self.max_x = 10000
 
         if self.domain == 'frequency':
             self.position = []
             self.current_drag_point = None
             self.drag_offset = None
             self.diameter = 8
-            self.mag_norm = 700
-            self.max_x = 10000
+            self.mag_norm = 600
+            self.max_x = 1500
 
         self.mouse_position_label = QLabel(self)
         self.mouse_position_label.move(10, 10)  
-        self.mouse_position_label.setText("Mouse Position: (0, 0)")
+        self.mouse_position_label.setText("(0, 0)                       ")
 
-        self.file_name = "output.wav" # sample name
+        self.file_name = "output.wav"
 
         self.pyo_server = parent.pyo_server
         self.notes = parent.notes
@@ -147,6 +172,7 @@ class Canvas(QWidget):
         
         if self.domain == "frequency":
             painter = QPainter(self)
+            painter.setFont(QFont("Arial", 12))
             painter.setPen(self.pen)
             painter.drawPath(self.path)
             
@@ -173,8 +199,8 @@ class Canvas(QWidget):
                 x, y = point
                 painter.setBrush(Qt.black)
                 painter.drawEllipse(QPoint(x, y), self.diameter, self.diameter)
-                text_position = QPoint(x + 10, y - 10)
-                painter.drawText(text_position, f"({int(round(x * self.max_x / self.width(), 0))}, {(self.height() - y)/self.mag_norm})")
+                text_position = QPoint(x, y - 30)
+                painter.drawText(text_position, f"({self.x_transform(x)}, {self.y_transform(y)})")
 
                 painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
                 painter.drawLine(x, y, x, self.height())
@@ -210,8 +236,8 @@ class Canvas(QWidget):
                     self.add_point(event.pos())
 
         if self.domain == "frequency":
-            x = int(round(event.x() * self.max_x / self.width(), 0))
-            y = (self.height() - event.y())/self.mag_norm
+            x = self.x_transform(event.x())
+            y = self.y_transform(event.y())
             self.mouse_position_label.setText(f"({x}, {y})")
 
             if self.current_drag_point is not None:
@@ -309,9 +335,8 @@ class Canvas(QWidget):
         if self.domain == "frequency":
             self.parent.tab_widget.setTabEnabled(0, False)
 
-            data_points = [(int(round(x * self.max_x / self.width(), 0)), (self.height() - y)/self.mag_norm) for x, y in self.position]
+            data_points = [(self.x_transform(x), self.y_transform(y)) for x, y in self.position]
 
-            #######################################################################
             N = 44100
             period = 2*np.pi 
             sampling_freq = N
@@ -330,10 +355,9 @@ class Canvas(QWidget):
             waveform = np.tile(waveform, duration) 
             waveform = waveform / np.max(waveform)
             
-            ###########################################################################
             signal_int16 = np.int16(waveform * 32767)
             write(self.file_name, sampling_freq, signal_int16)
-            self.show_fft(waveform, sampling_freq)
+            #self.show_fft(waveform, sampling_freq)
 
             self.pyo_server.start()
             self.start_synth(fund_freq, loop=True)
@@ -380,6 +404,18 @@ class Canvas(QWidget):
         plt.title('FFT of Waveform (Positive Frequencies)')
         plt.grid(True)
         plt.show()
+
+    def x_transform(self, x):
+        return int(round(x * self.max_x / self.width(), 0))
+    
+    def x_transform_inverse(self, x):
+        return int(round((x * self.width() / self.max_x)))
+
+    def y_transform(self, y):
+        return round((self.height() - y)/self.mag_norm, 3)
+
+    def y_transform_inverse(self, y):
+        return int(round(self.height() - (y * self.mag_norm)))
 
 def main():
     app = QApplication(sys.argv)
